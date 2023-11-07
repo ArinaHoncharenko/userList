ee9aa10479c16459a4b538b060c021c235e5f3cb
ArinaHoncharenko <118198844+ArinaHoncharenko@users.noreply.github.com>
2023-11-07 20:54:29 +0200

    Update UserController.cs

--- UserController.cs
+++ UserController.cs
@@ -11,18 +11,20 @@ namespace oop6_userList.Controllers
 
         public UserController()
         {
+            // Ініціалізація списку користувачів
             userList = new List<User>
             {
-               new User { Id = 0001, FirstName = "Ольга", LastName = "Богомолова", Email = "olha@example.com", RegistrationDate = new DateTime(2020, 5, 12) },
-               new User { Id = 0002, FirstName = "Олександр", LastName = "Синенко", Email = "oleksandr@example.com", RegistrationDate = new DateTime(2021, 8, 10) },
-               new User { Id = 0003, FirstName = "Петро", LastName = "Польовий", Email = "petro@example.com", RegistrationDate = new DateTime(2022, 12, 23) },
-               new User { Id = 0004, FirstName = "Олег", LastName = "Косенко", Email = "oleh@example.com", RegistrationDate = new DateTime(2021, 04, 08) },
-               new User { Id = 0005, FirstName = "Станіслав", LastName = "Триліс", Email = "stanislav@example.com", RegistrationDate = new DateTime(2022, 01, 29) },
+                new User { Id = 1, FirstName = "Ольга", LastName = "Богомолова", Email = "olha@example.com", RegistrationDate = new DateTime(2020, 5, 12) },
+                new User { Id = 2, FirstName = "Олександр", LastName = "Синенко", Email = "oleksandr@example.com", RegistrationDate = new DateTime(2021, 8, 10) },
+                new User { Id = 3, FirstName = "Петро", LastName = "Польовий", Email = "petro@example.com", RegistrationDate = new DateTime(2022, 12, 23) },
+                new User { Id = 4, FirstName = "Олег", LastName = "Косенко", Email = "oleh@example.com", RegistrationDate = new DateTime(2021, 04, 08) },
+                new User { Id = 5, FirstName = "Станіслав", LastName = "Триліс", Email = "stanislav@example.com", RegistrationDate = new DateTime(2022, 01, 29) },
             };
         }
 
         public IActionResult Index()
         {
+            // Відображення списку користувачів
             return View("Index", userList);
         }
 
@@ -31,8 +33,10 @@ namespace oop6_userList.Controllers
             User? user = userList.FirstOrDefault(u => u.Id == id);
             if (user == null)
             {
-                return NotFound(); 
+                // Повернення статусу 404 (Not Found) у випадку, якщо користувача не знайдено
+                return NotFound();
             }
+            // Відображення докладної інформації про користувача
             return View(user);
         }
     }
