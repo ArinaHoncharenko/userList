851475f782c1c8afdf00372b17082a720309a09d
ArinaHoncharenko <118198844+ArinaHoncharenko@users.noreply.github.com>
2023-11-10 18:05:11 +0200

    Update UserController.cs

--- UserController.cs
+++ UserController.cs
@@ -1,45 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using oop6_userList.Models;
-using System.Diagnostics;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace oop6_userList.Controllers
 {
     public class UserController : Controller
     {
-        private List<User> userList;
-
-        public UserController()
+        private static List<User> userList = new List<User>
         {
-            // Ініціалізація списку користувачів
-            userList = new List<User>
-            {
-                new User { Id = 1, FirstName = "Ольга", LastName = "Богомолова", Email = "olha@example.com", RegistrationDate = new DateTime(2020, 5, 12) },
-                new User { Id = 2, FirstName = "Олександр", LastName = "Синенко", Email = "oleksandr@example.com", RegistrationDate = new DateTime(2021, 8, 10) },
-                new User { Id = 3, FirstName = "Петро", LastName = "Польовий", Email = "petro@example.com", RegistrationDate = new DateTime(2022, 12, 23) },
-                new User { Id = 4, FirstName = "Олег", LastName = "Косенко", Email = "oleh@example.com", RegistrationDate = new DateTime(2021, 04, 08) },
-                new User { Id = 5, FirstName = "Станіслав", LastName = "Триліс", Email = "stanislav@example.com", RegistrationDate = new DateTime(2022, 01, 29) },
-            };
-        }
+            new User { Id = 1, FirstName = "Ольга", LastName = "Богомолова", Email = "olha@example.com", RegistrationDate = new DateTime(2020, 5, 12) },
+            new User { Id = 2, FirstName = "Олександр", LastName = "Синенко", Email = "oleksandr@example.com", RegistrationDate = new DateTime(2021, 8, 10) },
+            new User { Id = 3, FirstName = "Петро", LastName = "Польовий", Email = "petro@example.com", RegistrationDate = new DateTime(2022, 12, 23) },
+            new User { Id = 4, FirstName = "Олег", LastName = "Косенко", Email = "oleh@example.com", RegistrationDate = new DateTime(2021, 4, 8) },
+            new User { Id = 5, FirstName = "Станіслав", LastName = "Триліс", Email = "stanislav@example.com", RegistrationDate = new DateTime(2022, 1, 29) },
+        };
 
-        public IActionResult Index()
-        {
-            // Відображення списку користувачів
-            return View("Index", userList);
-        }
-
-        public IActionResult UserDetails(int id)
-        {
-            User? user = userList.FirstOrDefault(u => u.Id == id);
-            if (user == null)
-            {
-                // Повернення  404 
-                return NotFound();
-            }
-            // Відображення докладної інформації про користувача
-            return View(user);
-        }
-
+        // Додавання користувача
         [HttpGet]
         public IActionResult AddUser()
         {
@@ -54,12 +32,29 @@ namespace oop6_userList.Controllers
                 newUser.Id = userList.Max(u => u.Id) + 1;
                 newUser.RegistrationDate = DateTime.Now;
                 userList.Add(newUser);
-                return View("Index", userList);
+
+                // Перенаправлення на сторінку деталей нового користувача
+                return RedirectToAction("Index");
             }
+
             return View(newUser);
         }
 
+        // Відображення списку користувачів
+        public IActionResult Index()
+        {
+            return View(userList);
+        }
 
-
+        // Відображення деталей користувача
+        public IActionResult UserDetails(int id)
+        {
+            User? user = userList.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
     }
 }
